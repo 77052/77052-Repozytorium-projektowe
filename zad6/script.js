2197f3896be8cf2cbc44f314446985b95e59fb22
77052 <pmalczy1@stu.vistula.edu.pl>
2026-04-26 21:03:15 +0200

    Poprawa nazw w script.js

--- zad6/script.js
+++ zad6/script.js
@@ -85,7 +85,7 @@ fetch('./data.json')
     .then(response => response.json())
     .then(data => {
 
-        const UmiejętnościList = document.getElementById('UmiejętnościList');
+        const skillsList = document.getElementById('skillsList');
 
         data.umiejetnosci.forEach(skill => {
             const li = document.createElement('li');
@@ -93,7 +93,7 @@ fetch('./data.json')
             skillsList.appendChild(li);
         });
 
-        const ProjektyList = document.getElementById('ProjektyList');
+        const projectsList = document.getElementById('projectsList');
 
         data.projekty.forEach(project => {
             const li = document.createElement('li');
